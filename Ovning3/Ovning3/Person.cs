f9c380c395f8349c8adfb3b4d0bd1d07943c6781
k-sten <stenen@gmail.com>
2020-03-19 16:07:35 +0100

    1

---
+++ Ovning3/Ovning3/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ovning3
+{
+    class Person
+    {
+        private int age;
+        private string fName;
+        private string lName;
+        private double height;
+        private double weight;
+
+        public int Age { get => age; set => age = value; }
+        public string FName { get => fName; set => fName = value; }
+        public string LName { get => lName; set => lName = value; }
+        public double Height { get => height; set => height = value; }
+        public double Weight { get => weight; set => weight = value; }
+    }
+}
---
+++ Ovning3/Ovning3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ovning3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Person kalle = new Person();
+            //kalle.Age = 12;
+                        
+            PersonHandler personHandler = new PersonHandler();
+
+            Person Anders = personHandler.CreatePerson(20, "Anders", "Andersson", 180, 70);
+            Person Sara = personHandler.CreatePerson(14, "Sara", "Nilsson", 155, 45);
+            Person Johan = personHandler.CreatePerson(60, "Johan", "Mört", 175, 68);
+
+            personHandler.SetAge(Sara, 15);
+            personHandler.SetWeight(Johan, 72);
+            personHandler.SetLName(Anders, "Karlsson");
+        }
+    }
+}
